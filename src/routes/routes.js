9811d80e9d5cd219bdce7836016ec5456e616f19
daisyndungu <daisywndungu@gmail.com>
2018-08-01 01:02:48 +0300

    add routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const Movie = require('../controllers/movies');
+
+router.get('', (req, res) => {
     return res.status(200).send({message: "Welcome To API"});
 });
 
+router.post('/movies', Movie.addNewMovieRecord);
+router.get('/movies', Movie.displayAllMovies);
+router.get('/movies/:id', Movie.getMovieByID);
+router.put('/movies/:id', Movie.editMovieRecord);
+router.delete('/movies/:id', Movie.deleteMovieByID);
+router.delete('/movies', Movie.deleteByCategory);
+router.get('/movies/actors/:actor', Movie.getAllMoviesByActor)
 module.exports = router;
