742b8ae78abc86a498eb3afc2bc57c362362d4e9
daisyndungu <daisywndungu@gmail.com>
2018-08-01 01:00:36 +0300

    add movie model
    - add movie schema
    - add file extension and main actor validators

---
+++ src/models/movieModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const pathValidator = (pathName) => {
+    const extension = pathName.split(".");
+    return extension.includes("png") || extension.includes("jpg");
+}
+
+const arrayValidator = (mainActors) => {
+    return mainActors.length < 6 && mainActors.length != 0;
+}
+
+const movie = new Schema({
+    title: {type: String, required: true, unique: true},
+    description: {type: String, required: true},
+    mainActors: {type: Array, required: true, validate: [arrayValidator, "Main Actors can not be more than 5 or less 1"]},
+    poster: {type: String, required: true, validate: [pathValidator, "png or jpg not in file path"]},
+    category: {type: String, required: true}
+});
+
+module.exports = mongoose.model('movies', movie);
