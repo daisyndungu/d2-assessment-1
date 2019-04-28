9a8c7371b2f5988c3f51642f948876aa50e6c8bd
daisyndungu <daisywndungu@gmail.com>
2019-04-28 06:34:01 +0300

    Add new fields in movies model

--- src/models/movieModel.js
+++ src/models/movieModel.js
@@ -16,7 +16,11 @@ const movie = new Schema({
     description: {type: String, required: true},
     mainActors: {type: Array, required: true, validate: [arrayValidator, "Main Actors can not be more than 5 or less 1"]},
     poster: {type: String, required: true, validate: [pathValidator, "png or jpg not in file path"]},
-    category: {type: String, required: true}
+    category: {type: String, required: true},
+    startDate: {type: Date, required: true},
+    endDate: {type: Date, required: true},
+    showingNo: {type: Number, default: 0},
+    showingTime: {type: Array}
 });
 
 module.exports = mongoose.model('movies', movie);
